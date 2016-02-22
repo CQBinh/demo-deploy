891419eb6432970c6758a9f1f0441118f09b8f52
Cao Quang Binh <binhcq@asiantech.vn>
2016-02-22 16:07:02 +0700

    [Update] remove rvm call

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -14,7 +14,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/www/demo_deploy/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; ~/.rvm/bin/rvm exec bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=deploy
 set -u
 
